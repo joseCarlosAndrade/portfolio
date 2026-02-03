ebc1929fafff9ebc4604be0e52f982c21c54edc8
Jose Carlos <josecarlos.101@usp.br>
2026-02-03 10:38:40 -0300

    feat: done first version

--- js/main.js
+++ js/main.js
@@ -11,15 +11,23 @@
     .then(function (projects) {
       container.innerHTML = projects.map(function (p) {
         const techList = (p.tech || []).map(function (t) { return '<li>' + escapeHtml(t) + '</li>'; }).join('');
-        const linkHtml = p.link
-          ? '<a href="' + escapeHtml(p.link) + '" class="project-link" target="_blank" rel="noopener">View</a>'
-          : '';
+        const iconBase = 'https://cdn.simpleicons.org';
+        const githubIcon = '<img src="' + iconBase + '/github/8b9cb3" alt="" class="project-link-icon" width="16" height="16" loading="lazy">';
+        const ytIcon = '<img src="' + iconBase + '/youtube/8b9cb3" alt="" class="project-link-icon" width="16" height="16" loading="lazy">';
+        var links = [];
+        if (p.link) {
+          links.push('<a href="' + escapeHtml(p.link) + '" class="project-link" target="_blank" rel="noopener">' + githubIcon + ' View</a>');
+        }
+        if (p.youtube) {
+          links.push('<a href="' + escapeHtml(p.youtube) + '" class="project-link" target="_blank" rel="noopener">' + ytIcon + ' Video</a>');
+        }
+        const linkHtml = links.length ? '<p class="project-links">' + links.join('') + '</p>' : '';
         return (
           '<article class="project-card">' +
             '<h3 class="project-title">' + escapeHtml(p.title) + '</h3>' +
             '<p class="project-description">' + escapeHtml(p.description) + '</p>' +
             '<ul class="project-tech">' + techList + '</ul>' +
-            (linkHtml ? '<p class="project-links">' + linkHtml + '</p>' : '') +
+            linkHtml +
           '</article>'
         );
       }).join('');
